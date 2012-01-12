7fcc750078e55175ba0b1759d6d5a9c189f443a6
Eric Blossom <eric@BlossomAssociates.net>
2012-01-11 22:28:00 -0800

    Take key into account (when specified in the HTML).

--- Transposer.js
+++ Transposer.js
@@ -13,13 +13,65 @@
      */
     $.fn.elb_music_transpose = function (theHalfSteps) {
         var chordLetters = 'C D EF G A B',
-            chords = ['C', 'D\u266d', 'D', 'E\u266d', 'E', 'F', 'F\u266f', 'G', 'A\u266d', 'A', 'B\u266d', 'B'],
+            majorChords = ['C', 'D\u266d', 'D', 'E\u266d', 'E', 'F', 'F\u266f', 'G', 'A\u266d', 'A', 'B\u266d', 'B'],
             minorChords = ['C', 'C\u266f', 'D', 'D\u266f', 'E', 'F', 'F\u266f', 'G', 'G\u266f', 'A', 'B\u266d', 'B'],
-            aChord = /[A-G][#b\u266d\u266f]?/;
+            chordsInKey = { // Note: only need cannonical key names.
+                'C':   ['C',  'x',  'D',  'x',  'E',  'F',  'x',  'G',  'x',  'A',  'x',  'B'],
+                'Am':  ['C',  'x',  'D',  'x',  'E',  'F',  'x',  'G',  'x',  'A',  'x',  'B'],
+                'G':   ['C',  'x',  'D',  'x',  'E',  'x',  'F♯', 'G',  'x',  'A',  'x',  'B'],
+                'Em':  ['C',  'x',  'D',  'x',  'E',  'x',  'F♯', 'G',  'x',  'A',  'x',  'B'],
+                'D':   ['x',  'C♯', 'D',  'x',  'E',  'x',  'F♯', 'G',  'x',  'A',  'x',  'B'],
+                'Bm':  ['x',  'C♯', 'D',  'x',  'E',  'x',  'F♯', 'G',  'x',  'A',  'x',  'B'],
+                'A':   ['x',  'C♯', 'D',  'x',  'E',  'x',  'F♯', 'x',  'G♯', 'A',  'x',  'B'],
+                'F♯m': ['x',  'C♯', 'D',  'x',  'E',  'x',  'F♯', 'x',  'G♯', 'A',  'x',  'B'],
+                'E':   ['x',  'C♯', 'x',  'D♯', 'E',  'x',  'F♯', 'x',  'G♯', 'A',  'x',  'B'],
+                'D♭m': ['x',  'C♯', 'x',  'D♯', 'E',  'x',  'F♯', 'x',  'G♯', 'A',  'x',  'B'],
+                'B':   ['x',  'C♯', 'x',  'D♯', 'E',  'x',  'F♯', 'x',  'G♯', 'x',  'A♯', 'B'],
+                'G♯m': ['x',  'C♯', 'x',  'D♯', 'E',  'x',  'F♯', 'x',  'G♯', 'x',  'A♯', 'B'],
+                'F♯':  ['x',  'C♯', 'x',  'D♯', 'x',  'E♯', 'F♯', 'x',  'G♯', 'x',  'A♯', 'B'],
+                'D♯m': ['x',  'C♯', 'x',  'D♯', 'x',  'E♯', 'F♯', 'x',  'G♯', 'x',  'A♯', 'B'],
+                'C♯':  ['B♯', 'C♯', 'x',  'D♯', 'x',  'E♯', 'F♯', 'x',  'G♯', 'x',  'A♯', 'x'],
+                'A♯m': ['B♯', 'C♯', 'x',  'D♯', 'x',  'E♯', 'F♯', 'x',  'G♯', 'x',  'A♯', 'x'],
+                'C♭':  ['x',  'D♭', 'x',  'E♭', 'F♭', 'x',  'G♭', 'x',  'A♭', 'x',  'B♭', 'C♭'],
+                'A♭m': ['x',  'D♭', 'x',  'E♭', 'F♭', 'x',  'G♭', 'x',  'A♭', 'x',  'B♭', 'C♭'],
+                'G♭':  ['x',  'D♭', 'x',  'E♭', 'x',  'F',  'G♭', 'x',  'A♭', 'x',  'B♭', 'C♭'],
+                'E♭m': ['x',  'D♭', 'x',  'E♭', 'x',  'F',  'G♭', 'x',  'A♭', 'x',  'B♭', 'C♭'],
+                'D♭':  ['C',  'D♭', 'x',  'E♭', 'x',  'F',  'G♭', 'x',  'A♭', 'x',  'B♭', 'x'],
+                'B♭m': ['C',  'D♭', 'x',  'E♭', 'x',  'F',  'G♭', 'x',  'A♭', 'x',  'B♭', 'x'],
+                'A♭':  ['C',  'D♭', 'x',  'E♭', 'x',  'F',  'x',  'G',  'A♭', 'x',  'B♭', 'x'],
+                'Fm':  ['C',  'D♭', 'x',  'E♭', 'x',  'F',  'x',  'G',  'A♭', 'x',  'B♭', 'x'],
+                'E♭':  ['C',  'x',  'D',  'E♭', 'x',  'F',  'x',  'G',  'A♭', 'x',  'B♭', 'x'],
+                'Cm':  ['C',  'x',  'D',  'E♭', 'x',  'F',  'x',  'G',  'A♭', 'x',  'B♭', 'x'],
+                'B♭':  ['C',  'x',  'D',  'E♭', 'x',  'F',  'x',  'G',  'x',  'A',  'B♭', 'x'],
+                'Gm':  ['C',  'x',  'D',  'E♭', 'x',  'F',  'x',  'G',  'x',  'A',  'B♭', 'x'],
+                'F':   ['C',  'x',  'D',  'x',  'E',  'F',  'x',  'G',  'x',  'A',  'B♭', 'x'],
+                'Dm':  ['C',  'x',  'D',  'x',  'E',  'F',  'x',  'G',  'x',  'A',  'B♭', 'x'],
+            },
+            chords = majorChords,
+            aChord = /[A-G][#b\u266d\u266f]?/,
+            key,
+            keyName = function(sloppyName) {
+                sloppyName = sloppyName.replace('#', '♯').replace('b', '♭');
+                if (sloppyName[0].match('[a-g]')) {
+                    return sloppyName.toUpperCase() + 'm';
+                }
+                return sloppyName;
+            };
         return this.each(function (theIndex, theElement) {
             var h = this.innerHTML,
-                m = h.match(aChord),
+                m,
                 x;
+            if ($(this).hasClass('key')) {
+                h = keyName(h);
+                if (h[h.length-1] === 'm' ) {
+                    chords = minorChords;
+                }
+                else {
+                    chords = majorChords;
+                }
+                key = h;
+            }
+            m = h.match(aChord);
             if (m) {
                 x = chordLetters.search(m[0][0]);
                 if (-1 < x) {
@@ -34,12 +86,25 @@
                         x += chordLetters.length;
                     }
                     x %= chordLetters.length;
-                    if (-1 < h.indexOf('m') && !h.match('maj|dom')) { // then it's a minor (or diminished) chord.
-                        this.innerHTML = h.replace(m[0], minorChords[x]);
+                    if (!key) { // then no key was given in the input.
+                        // Guess based on chord.
+                        if (-1 < h.indexOf('m') && !h.match('maj|dom')) { // then it's a minor (or diminished) chord.
+                            this.innerHTML = h.replace(m[0], minorChords[x]);
+                        }
+                        else { // it's a major chord.
+                            this.innerHTML = h.replace(m[0], majorChords[x]);
+                        }
                     }
-                    else { // it's a major chord.
+                    else { // the proper key has been set.
                         this.innerHTML = h.replace(m[0], chords[x]);
                     }
+                }
+            }
+            if ($(this).hasClass('key')) {
+                key = this.innerHTML;
+                chords = chordsInKey[key];
+                if (!chords) {
+                    alert("couldn't find key " + key);
                 }
             }
         });
